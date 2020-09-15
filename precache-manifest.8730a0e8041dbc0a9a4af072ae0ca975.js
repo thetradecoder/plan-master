dd21ebfe036cc61951b4458c0959103e98b7f910
thetradecoder <thetradecoder@gmail.com>
2020-09-15 11:31:34 +0600

    Updates

---
+++ precache-manifest.8730a0e8041dbc0a9a4af072ae0ca975.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8b87de77a194f548951259e590a90a",
+    "url": "/plan-master/index.html"
+  },
+  {
+    "revision": "0bb6b3b5d24db8b78fc3",
+    "url": "/plan-master/static/css/2.2f73783b.chunk.css"
+  },
+  {
+    "revision": "20d5c76696855e5276d0",
+    "url": "/plan-master/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0bb6b3b5d24db8b78fc3",
+    "url": "/plan-master/static/js/2.a1181371.chunk.js"
+  },
+  {
+    "revision": "580299e0d42264ba9f60b23f9b257e48",
+    "url": "/plan-master/static/js/2.a1181371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d5c76696855e5276d0",
+    "url": "/plan-master/static/js/main.0f27972a.chunk.js"
+  },
+  {
+    "revision": "87f48ea1325ee6508c9a",
+    "url": "/plan-master/static/js/runtime-main.c14fd464.js"
+  }
+]);
