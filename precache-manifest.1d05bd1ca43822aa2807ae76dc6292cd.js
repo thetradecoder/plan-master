b95d04fbd5735ff038f5cf1ee9a7e9901eaf6ffb
thetradecoder <thetradecoder@gmail.com>
2020-09-17 16:28:40 +0600

    Updates

--- precache-manifest.1d05bd1ca43822aa2807ae76dc6292cd.js
+++ precache-manifest.1d05bd1ca43822aa2807ae76dc6292cd.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "04afabfa3dfe9afab9cbcc4b835cd088",
+    "revision": "bc43db390a350340b84b6ea15e2433d5",
     "url": "/plan-master/index.html"
   },
   {
-    "revision": "c1db46d0941569f5e72a",
+    "revision": "22131d00ff95b2fe3085",
     "url": "/plan-master/static/css/2.2f73783b.chunk.css"
   },
   {
-    "revision": "fe7623a8d9498e7aefa7",
+    "revision": "3e7d8a0df146222d33a1",
     "url": "/plan-master/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "c1db46d0941569f5e72a",
-    "url": "/plan-master/static/js/2.a37511a8.chunk.js"
+    "revision": "22131d00ff95b2fe3085",
+    "url": "/plan-master/static/js/2.0e3beb44.chunk.js"
   },
   {
     "revision": "580299e0d42264ba9f60b23f9b257e48",
-    "url": "/plan-master/static/js/2.a37511a8.chunk.js.LICENSE.txt"
+    "url": "/plan-master/static/js/2.0e3beb44.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fe7623a8d9498e7aefa7",
-    "url": "/plan-master/static/js/main.bf718fe6.chunk.js"
+    "revision": "3e7d8a0df146222d33a1",
+    "url": "/plan-master/static/js/main.cef42d0f.chunk.js"
   },
   {
     "revision": "87f48ea1325ee6508c9a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/plan-master/precache-manifest.d5f564196b3349f5af5d663d29b66859.js"
+  "/plan-master/precache-manifest.1d05bd1ca43822aa2807ae76dc6292cd.js"
 );
 
 self.addEventListener('message', (event) => {
